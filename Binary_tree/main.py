bf6dcf68c505364dac753482e99fed70626dd302
Levirroh <johann.gr14@gmail.com>
2025-12-01 14:45:48 -0300

    feat: added binary tree folder

---
+++ Binary_tree/main.py
@@ -0,0 +1,37 @@
+class item:
+    def __init__(self, value, right, left):
+        self.value = value
+        self.left = left 
+        self.right = right 
+
+arvore = None
+  
+def main():
+  print("Olá, seja bem-vindo ao projeto de Árvore Binária, usando Python.")
+  
+  while(True):
+    print("Digite a operação que deseja realizar: \n \n \n")
+    print("1. Inserir : ")
+    print("2. Deletar : ")
+    print("3. Procurar: ")
+    alternativa = input("Opção (digite o número): ")
+
+    match(alternativa):
+      case "1":
+        InserirValor()
+      case "2":
+        ExcluirValor()
+      case "3":
+        ProcurarValor()
+
+def InserirValor():
+  if(arvore)
+  print("");
+def ExcluirValor():
+  print("");
+def ProcurarValor():
+  print("");
+  
+
+    
+    
